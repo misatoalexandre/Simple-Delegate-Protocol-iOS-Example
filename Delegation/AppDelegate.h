6d1aff533068070bea037029fe707eaa959fa4dc
misatoalexandre <misatotina.alexandre@gmail.com>
2013-08-08 00:17:19 -0400

    Initial Commit

---
+++ Delegation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Delegation
+//
+//  Created by Misato Tina Alexandre on 8/8/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Delegation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delegation
+//
+//  Created by Misato Tina Alexandre on 8/8/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
